6bb8f62cb9a07bf05274251dca280a9f096d538f
Lou Mauriege <lou.mauriege@laposte.net>
2024-07-07 19:37:04 +0200

    build: change interfaces by models

--- src/app/interfaces/previous-olympic-games-participations.ts
+++
@@ -1,7 +0,0 @@
-export interface PreviousOlympicGamesParticipations {
-    id: number,
-    year: number,
-    city: string,
-    medalsCount: number,
-    athleteCount: number
-}
--- src/app/interfaces/previous-olympic-games-statistics.ts
+++
@@ -1,7 +0,0 @@
-import { PreviousOlympicGamesParticipations } from "./previous-olympic-games-participations"
-
-export interface PreviousOlympicGamesStatistics {
-    id: number,
-    country: string,
-    participations: Array<PreviousOlympicGamesParticipations>
-}
