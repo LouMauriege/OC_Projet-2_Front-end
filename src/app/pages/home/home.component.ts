f1f09392624210f77575827c7f3ebfbdc3559358
Lou Mauriege <lou.mauriege@laposte.net>
2024-07-12 18:32:25 +0200

    build: subscribe observable + install ng2-chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
+import { Olympics } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
+  imports: [BaseChartDirective, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympics[] | null> = of(null);
+  public olympicsArray: Olympics[] | null = [];
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.olympics$.subscribe(olympic => this.olympicsArray = olympic);
   }
 }
