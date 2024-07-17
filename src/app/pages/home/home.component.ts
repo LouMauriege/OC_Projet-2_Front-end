1ed5aafa64b9ccf5a58ad22b2dd7b9e2d4d29763
Lou Mauriege <lou.mauriege@laposte.net>
2024-07-18 00:19:39 +0200

    build:work with chart.js

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { Olympics } from 'src/app/core/models/Olympic';
+import { Participations } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+
 import { BaseChartDirective } from 'ng2-charts';
 import { Chart } from 'chart.js/auto';
-import { Participations } from 'src/app/core/models/Participation';
 
 @Component({
   standalone: true,
@@ -18,31 +19,32 @@ export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympics[] | null> = of(null);
   public olympicsArray: Olympics[] | null = [];
   public chart: any;
-  public countriesIds: Array<Number> = [];
-  public countriesNames: Array<String> = [];
-  public countriesParticipations: Array<Number> = [];
+  public countriesIds: Array<number> = [];
+  public countriesNames: Array<string> = [];
+  public participationsArray: Array<Array<Participations>> = [];
+  public countryMedalsCount: Array<number> = [];
 
   constructor(private olympicService: OlympicService) {}
 
-  createChart(countriesNames: Array<String>, countriesParticipations: Array<Number>){
+  createChart(countriesNames: Array<String>, countriesMedalsCount: Array<Number>){
   
     this.chart = new Chart("MyChart", {
       type: 'pie',
-    data: {
-      labels: countriesNames,
-      datasets: [{
-        label: '# of Votes',
-        data: countriesParticipations,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+      data: {
+        labels: countriesNames,
+        datasets: [{
+          label: '# of Votes',
+          data: countriesMedalsCount,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
       }
-    }
     });
   }
 
@@ -51,13 +53,18 @@ export class HomeComponent implements OnInit {
 
     this.olympics$.subscribe(olympic => {
       this.olympicsArray = olympic;
-      console.log(this.olympicsArray);
       if (this.olympicsArray) {
         this.countriesIds = this.olympicsArray?.map(({ id }) => id);
         this.countriesNames = this.olympicsArray?.map(({ country }) => country);
-        this.countriesParticipations = this.olympicsArray?.map(({ participations }) => participations.length);
-        console.log(this.countriesIds, this.countriesNames, this.countriesParticipations);
-        this.createChart(this.countriesNames, this.countriesParticipations);
+        this.participationsArray = this.olympicsArray?.map(({ participations }) => participations);
+        for (let i = 0; i < this.participationsArray.length; i++) {
+          this.countryMedalsCount[i] = 0;
+          for (let j = 0; j < this.participationsArray[i].length; j++) {
+            this.countryMedalsCount[i] += this.participationsArray[i][j].medalsCount;
+          }
+        }
+
+        this.createChart(this.countriesNames, this.countryMedalsCount);
       }
     });
   }
